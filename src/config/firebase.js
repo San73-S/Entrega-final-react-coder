43544921a826e254cc460f2405907de1ed340709
San73-S <seba00001s@gmail.com>
2025-03-08 10:19:50 -0300

    cambio en base de datos

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+/*import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -11,4 +11,21 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);*/
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWcSfvO2p9iN-a9KV1VtBsuq5cK_NARoM",
+    authDomain: "breaking-store.firebaseapp.com",
+    projectId: "breaking-store",
+    storageBucket: "breaking-store.firebasestorage.app",
+    messagingSenderId: "82010205696",
+    appId: "1:82010205696:web:f50e4669de3b1d6d80f2e5",
+    measurementId: "G-ZCRSNNHDRY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
